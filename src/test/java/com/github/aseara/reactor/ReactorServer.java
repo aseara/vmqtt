0e9311f4193fd62794087a9ff8aeebf8ddc6bf39
aseara <aseara@163.com>
2022-12-28 10:10:09 +0800

    test: delete reactor test

--- src/test/java/com/github/aseara/reactor/ReactorServer.java
+++
@@ -1,9 +0,0 @@
-package com.github.aseara.reactor;
-
-public class ReactorServer {
-
-    public static void main(String[] args) throws Exception {
-        new Thread(new Reactor(8080), "reactor-001").start();
-    }
-
-}
